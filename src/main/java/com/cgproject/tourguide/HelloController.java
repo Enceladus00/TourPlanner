6e8f5c34735b1c9a70591935deb9999d49b87066
enceladus00 <melihkarakas61@gmail.com>
2025-02-20 13:42:56 +0100

    welcome message

---
+++ src/main/java/com/cgproject/tourguide/HelloController.java
@@ -0,0 +1,14 @@
+package com.cgproject.tourguide;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("--|---WELCOME---|--");
+    }
+}
